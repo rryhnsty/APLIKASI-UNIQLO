efa726ec894a5cdbac01899caac2a1ff5d3757c1
ZakiArfr <zakiarif2006@gmail.com>
2026-05-28 23:55:42 +0700

    Add files via upload

---
+++ front-end/Login/Product.java
@@ -0,0 +1,34 @@
+package SQLManagement.TugasAkhirBasdat.frontend;
+
+
+
+public class Product {
+
+    private String category;
+    private String name;
+    private String price;
+    private String imagePath;
+
+    public Product(String category, String name, String price, String imagePath) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.imagePath = imagePath;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+}
